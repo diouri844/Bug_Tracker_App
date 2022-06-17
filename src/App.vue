59863bfa5f339ab2b8cbbadcab436b1ca2403307
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-06-17 22:09:26 +0100

    setup commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <h1> bug tracer app by <span>Ch</span>__<span>B</span>uilder</h1>
+    <hr class="ligne">
+    <nav>
+      <button class="nav-item" v-on:click="update_btn_state"><i class="fa-solid fa-user-plus"></i></button>
+      <button class="nav-item" v-on:click="update_btn_state"><i class="fa-solid fa-right-to-bracket"></i></button>
+    </nav>
+    <login-form v-show="show_login"></login-form>
+    <RegistrationForm v-show="show_registration"></RegistrationForm>
+  </div>
 </template>
-
+<script>
+import loginForm from "@/components/LoginForm.vue"
+import RegistrationForm from "@/components/RegistrationForm.vue"
+export default {
+  components: {
+    loginForm,
+    RegistrationForm
+  },
+  data() {
+    return{
+      'show_login':true,
+      'show_registration':false
+    }
+  },
+  methods:{
+    update_btn_state(){
+      this.show_login=!this.show_login;
+      this.show_registration=!this.show_registration;
+      console.log(this.login,this.show_registration)
+    }
+  }
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Times, "Times New Roman", Georgia, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #c6d9dc;
+  background:linear-gradient(0deg,#5F1B2A,#0d0f0c 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-nav {
-  padding: 30px;
+.container {
+  background:#0d0f0c;
+  display: inline;
+  margin:10% auto;
+  width:60%;
+  height:600px;
+  border-radius: 25px;
+  box-shadow: 0px 7px 29px 0px rgba(100, 100, 111, 0.2) ;
+}
+.container h1{
+  text-transform: capitalize;
+  color: #c6d9dc;
+  letter-spacing: 2px;
+}
+span{
+  color:#238636;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.ligne{
+  margin-left:35%;
+  width: 50px;
+  height: 1px;
+  background-color: #238636;
+  border: none;
+  margin-top: -7px;
+  margin-bottom: 3%;
+}
+nav{
+  margin-bottom: 3%;
+}
+.nav-item{
+  padding: 10px 10px;
+  margin:10px 10px;
+  background:transparent;
+  color:#c6d9dc;
+  border-top:0px;
+  border-right:0px;
+  border-bottom:0px;
+  border-left:3px solid #238636; 
+  border-radius:10px;
+  font-size:17px;
+}
+.nav-item:hover{
+  border-left:3px solid #c6d9dc;
+  color: #238636;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="form">
+        <div class="header">
+            <h1>login <span>now</span></h1>
+        </div>
+        <div class="form_body">
+            <div class="body-item">
+                <label><i class="fa-solid fa-user"></i> user <span>name</span></label>
+                <input type="text" placeholder="Ch__open866">
+            </div>
+            <div class="body-item">
+                <label><i class="fa-solid fa-envelope"></i> user <span>email</span></label>
+                <input type="email" placeholder="Ch__open866@gmail.com">
+            </div>
+            <div class="body-item">
+                <label><i class="fa-solid fa-key"></i> user <span>password</span></label>
+                <input type="password" placeholder="****************">
+            </div>
+            <button class="login-btn">login <i class="fa-solid fa-arrow-right-to-bracket"></i> </button>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .form{
+        font-family: Times, "Times New Roman", Georgia, serif;
+        background:transparent;
+        padding:2px;
+        font-size: 14px;
+        margin:auto auto;
+        border:1px solid #c6d9dc;
+        border-radius: 15px;
+        width:300px;
+    }
+    .header{
+        text-transform: capitalize;
+        color:#0d0f0c;
+        
+    }
+    span{
+        color:#238636;
+    }
+    .login-btn{
+        padding: 10px 10px;
+        margin-bottom: 10px;
+        width: 250px;
+        text-align: center;
+        text-transform: capitalize;
+        border-radius: 5px;
+        border: 1px solid #c6d9dc;
+        background-color:#238636;
+        color: #c6d9dc;
+    }
+    .login-btn:hover{
+        background-color:#0d0f0c;
+        color:#c6d9dc;
+        border: 1px solid #c6d9dc; 
+    }
+    .body-item{
+        display:grid;
+        grid-template-rows: 1fr;
+        align-items: left;
+        margin:10px 0;
+    }
+    input{
+        width:80%;
+        margin-left:5%;
+        border-radius:25px;
+        border: 1px solid #c6d9dc;
+        padding: 10px 10px;
+        background:transparent;
+        color:#c6d9dc;
+    }
+    label{
+        font-size:17px;
+        text-transform: capitalize;
+        text-align: center;
+        margin-bottom: 10px;
+        color:#c6d9dc;
+    }
+</style>
