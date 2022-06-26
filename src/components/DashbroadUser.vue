7a6cb25a6869c2d8f58275c0796da7e76621884c
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-06-26 22:43:09 +0100

    update home components

--- src/components/DashbroadUser.vue
+++ src/components/DashbroadUser.vue
@@ -1,34 +1,56 @@
 <template>
   <div class="main">
-    <h1> Your dashbroad page {{ userName }} </h1>
+    <h1 class="main-title">
+      <i class="fas fa-house custom-element"></i>
+       Your Home  
+       <span class="custom-element"> {{ userName }} </span>
+    </h1>
+    <UserProjectList class="side-item"></UserProjectList>
   </div>
   
 </template>
 
 <script>
 // separt dashbroad to many components : teams list/current user projects-engagement:
+import UserProjectList from "@/components/UserProjectList.vue"
 export default {
-    props:['userName']
+    props:['userName'],
+    components:{
+      UserProjectList
+    }
 }
 </script>
 
 <style scoped>
 .main{
-  /*
-  background-color: #0cbaba;
-  background-image: linear-gradient(315deg, #0cbaba 0%, #380036 74%);
-  */
-  /*
-  background-color: #63a4ff;
-  background-image: linear-gradient(315deg, #63a4ff 0%, #83eaf1 74%);
-  */
-  background-color: #537895;
-  background-image: linear-gradient(315deg, #537895 0%, #09203f 74%);
-  position:absolute;
   top:0px;
   right:0px;
   bottom:0px;
   left:0px;
+  background:linear-gradient(to right,#E8126B,#0e0e0e);
+  position:absolute;
+  display:grid;
+  grid-template-columns:repeat(12,1fr);
+  grid-template-rows:repeat(12,1fr);
 }
 
+.main-title{
+  margin: 1% auto;
+  width: 50%;
+  border-bottom: 2px solid #537895;
+  color:#fff;
+  padding: 10px;
+  text-align: center;
+  grid-column: 1/ span 11;
+  grid-row: span 1;
+}
+
+.custom-element{
+  font-family: fantasy;
+  color: #537895;
+}
+.side-item{
+  grid-column: 2/ span 10 ;
+  grid-row: 3/ span 5;
+}
 </style>
---
+++ src/components/UserProjectList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="content-side">
+        <div class="content-header">
+            <h2>
+                <i class="fas fa-folder"></i> your project 
+            </h2>
+        </div>
+    </div>    
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+.content-side{
+    margin:0;
+    padding:5px 15px;
+    border: 1px solid #537895;
+    background:linear-gradient(to right,#E8126B,#0e0e0e);
+    border-radius: 18px;
+    color:#fff;
+
+}
+h2{
+    text-transform: capitalize;
+    text-decoration: underline;
+    text-decoration-color: #537895;
+}
+
+.custom-element{
+  font-family: fantasy;
+  color: #537895;
+}
+</style>
