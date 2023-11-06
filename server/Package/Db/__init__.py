430f9e2cb1e9c9199494c982891a09ea1ac715dd
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-06 20:20:38 +0100

    Update db setup

--- server/Package/Db/__init__.py
+++ server/Package/Db/__init__.py
@@ -1,3 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
+import uuid 
+from sqlalchemy.dialects.postgresql import UUID
+
+
+
 
 my_database = SQLAlchemy()
+
+
+
+def setup_user_team_role():
+# Define a many-to-many association table for User and Team with roles
+    user_team_role = my_database.Table(
+        'user_team_role',
+        my_database.Column('user_id', 
+            UUID(as_uuid=True), 
+            my_database.ForeignKey('user.id'), 
+            primary_key=True),
+        my_database.Column('team_id', 
+             UUID(as_uuid=True), 
+            my_database.ForeignKey('team.id'), 
+            primary_key=True),
+        my_database.Column('role', 
+            my_database.String(20), 
+            nullable=False
+        )  # You can define roles as needed
+    )
+    return
