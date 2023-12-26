311f7227b5f8039a10847f4475331112464f04c7
Chopen <75852177+diouri844@users.noreply.github.com>
2023-12-26 20:09:58 +0100

    Update Team model

--- server/Package/Models/Team.py
+++ server/Package/Models/Team.py
@@ -8,7 +8,9 @@ class Team(my_database.Model):
     id = my_database.Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
     name = my_database.Column(my_database.String(80), unique=True, nullable=False)
     admin = my_database.Column(UUID(as_uuid=True),nullable=False)
-    #projects = my_database.relationship('Project', back_populates='team')
+    projects = my_database.relationship('Project', 
+                                        secondary="team_project",
+                                        back_populates='teams')
     users = my_database.relationship('User', 
                                     secondary='user_team_role', 
                                     back_populates='teams'
