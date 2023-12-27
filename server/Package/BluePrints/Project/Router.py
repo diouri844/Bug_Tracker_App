995e126db7d1e5b7c22f5ffa9f4e6b939359a366
Chopen <75852177+diouri844@users.noreply.github.com>
2023-12-27 19:14:42 +0100

    Update project router

--- server/Package/BluePrints/Project/Router.py
+++ server/Package/BluePrints/Project/Router.py
@@ -12,8 +12,26 @@ project_api = Blueprint("project_api", __name__)
 Prefixer = API_PREFIXER+Project_PREFIXER
 
 
-
+#test ping :
 @project_api.route(Prefixer+"/Ping", methods=["GET"])
 @cross_origin()
 def ping_handler():
-        return jsonify({"Response": "Team Pong"}),200
+        return jsonify({"Response": "Project Pong"}),200
+
+#get all project list :
+@project_api.route(Prefixer+"/List", methods=["GET"])
+@cross_origin()
+def get_project_list():
+        #get metadata from request params :
+        page = int(request.args.get("page", 1))
+        per_page = int(request.args.get("per_page",10))
+        #call static method to get project list:
+        projectList = ProjectService.get_project_list(page,per_page)
+        return jsonify({"data": projectList}),200
+
+#get a project by id :
+@project_api.route(Prefixer+"/<project_id>", methods=["GET"])
+@cross_origin()
+def get_project_by_id(project_id):
+        return jsonify()
+
