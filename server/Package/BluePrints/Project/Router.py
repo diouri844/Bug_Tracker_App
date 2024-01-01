ba2db8cccf63878a379f1e62e62009882d39fba6
Chopen <75852177+diouri844@users.noreply.github.com>
2024-01-01 19:23:51 +0100

    update project router

--- server/Package/BluePrints/Project/Router.py
+++ server/Package/BluePrints/Project/Router.py
@@ -6,6 +6,8 @@ from flask import request, jsonify, Blueprint
 # import flask cors configuration lib : 
 from flask_cors import cross_origin
 
+from Package.BluePrints.User.Service import UserService
+
 
 
 
@@ -74,4 +76,43 @@ def get_project_manager(project_id):
 @project_api.route(Prefixer+"/TeamList/<project_id>", methods=["GET"])
 @cross_origin()
 def get_prject_team_list(project_id):
-        return jsonify(),200
+        #check if the project exist or not : 
+        projectTarget = ProjectService.get_project_by_id(project_id)
+        if not projectTarget:
+                return jsonify({"message": "Project not found"}),404
+        #get the alist of teams id that workin on this project :
+        project_team_list = ProjectService.get_project_team_list(project_id)
+        return jsonify({"data": project_team_list}),200
+
+#update project manager by id :
+@project_api.route(Prefixer+"/<project_id>/Manger/New/<user_id>", methods=["PUT"])
+@cross_origin()
+def update_project_manager(project_id, user_id):
+        #check if project exist :
+        projectTarget = ProjectService.get_project_by_id( project_id )
+        if not projectTarget: return jsonify({"message": "Project not found"}),404
+        #check if the user exist :
+        userTarget = UserService.alreadyExists( user_id )
+        if not userTarget: return jsonify({"message": "User not found"}),404
+        #call the static service to set the new project manager :
+        updateManagerStatus = ProjectService.update_project_manager(project_id, user_id)
+        if not updateManagerStatus: return jsonify({"message": "Faild to Update Project manager"}),500
+        return jsonify({"message": "Manger Updated Successufully"}),202
+
+#update project state by project id :
+@project_api.route(Prefixer+"/<project_id>/State",methods=["PUT"])
+@cross_origin()
+def update_project_state(project_id):
+        #check if project exist :
+        projectTarget = ProjectService.get_project_by_id( project_id )
+        if not projectTarget: return jsonify({"message": "Project not found"}),404
+        #get the new state from request  body :
+        paylaod = request.json
+        if "state" in paylaod:
+                newState = paylaod["state"]
+                #update process :
+                stateUpdated = ProjectService.update_project_status(project_id, newState)
+                if not stateUpdated:
+                        return jsonify({"message": "Failed to update project state"}),500
+                return jsonify({"message": "Project status updated successfully"}),202
+        return jsonify({"message": "New State is required"}),400
