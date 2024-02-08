a212da5afc9f131f71b4444efbabe30c97bca511
Chopen <75852177+diouri844@users.noreply.github.com>
2024-02-08 20:26:38 +0100

    update ticket router

--- server/Package/BluePrints/Ticket/Router.py
+++ server/Package/BluePrints/Ticket/Router.py
@@ -1,4 +1,5 @@
 from Package.Models.Ticket import Ticket
+from Package.BluePrints.Ticket.Service import TicketService
 from Package.Db import my_database
 from Package.BluePrints import API_PREFIXER, Ticker_PREFIXER
 from flask import request, jsonify , Blueprint
@@ -14,3 +15,40 @@ Prefixer = API_PREFIXER + Ticker_PREFIXER
 @cross_origin()
 def ping_handler():
     return jsonify({"Response": "Ticket Pong"}),200
+
+
+#get ticket list by status :
+@ticket_api.route(Prefixer+"/list", methods=["GET"])
+@cross_origin()
+def get_tcket_list():
+    #get query parameters :
+    page = int(request.args.get("page", 1))
+    per_page = int(request.args.get("per_page",10))
+    #target state of ticket needed : opened , cloded :
+    status = request.args.get("status", "open")
+    #call static service to get the ticket list :
+    tickerList = TicketService.get_ticket_list(
+        page=page,
+        per_page=per_page,
+        status=status
+    )
+    return jsonify({"data":tickerList}),200
+
+
+#get ticket dtails by id :
+@ticket_api.route(Prefixer+"/<ticket_id>/details", methods=["GET"])
+@cross_origin()
+def getTicketDtails(ticket_id):
+    #call static method to get ticket dtails by ticket id or none :
+    targetObject = TicketService.get_ticket_details(ticket_id)
+    if not targetObject["state"]:
+        return jsonify({"message": "Ticket not found"}),404
+    return jsonify({"data":targetObject["data"]}),201
+
+
+#get a project related ticket list :
+@ticket_api.route(Prefixer+"/Projects/<project_id>/tickets",methods=["GET"])
+@cross_origin()
+def getProjectsTicket(project_id):
+    #ckeck if the project exist : 
+    return jsonify(),201
