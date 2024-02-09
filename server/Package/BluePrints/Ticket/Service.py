0f24cb12d3c09af56338ccabbcce736f815fdf10
Chopen <75852177+diouri844@users.noreply.github.com>
2024-02-09 20:10:55 +0100

    update ticket service

--- server/Package/BluePrints/Ticket/Service.py
+++ server/Package/BluePrints/Ticket/Service.py
@@ -41,3 +41,42 @@ class TicketService:
             print("Error getting ticket details ", e)
             response["state"] = False
         return response
+    @staticmethod
+    def get_project_ticket(project_id, page=1, per_page=10):
+        TicketList = Ticket.query.filter(
+            project_id==project_id
+        ).paginate(
+            page=page,
+            per_page=per_page,
+            error_out=False
+        )
+        #format data :
+        formated_data = {
+            "items": [ ticker.toDict() for ticker in TicketList],
+            "page":TicketList.page,
+            "per_page":TicketList.per_page,
+            "total_pages":TicketList.pages,
+            "total_items":TicketList.total
+        }
+        return formated_data
+    @staticmethod
+    def get_user_ticket(user_id,page=1,per_page=10, status="All"):
+        TickerList = Ticket.query.filter(
+            and_(
+                Ticket.assigned_user_id==user_id,
+                Ticket.status==status
+            )
+        ).paginate(
+            page=page,
+            per_page=per_page,
+            error_out=False
+        )
+        #format data : 
+        formated_data = {
+            "items": [ item.toDict() for item in TickerList.items ],
+            "page":TickerList.page,
+            "per_page":TickerList.per_page,
+            "total_pages":TickerList.pages,
+            "total_items":TickerList.total
+        }
+        return formated_data
