95d628f3cc2fedfc780a9355395af49a3b522b3a
Chopen <75852177+diouri844@users.noreply.github.com>
2024-02-09 20:10:28 +0100

    update ticker router

--- server/Package/BluePrints/Ticket/Router.py
+++ server/Package/BluePrints/Ticket/Router.py
@@ -1,5 +1,7 @@
 from Package.Models.Ticket import Ticket
 from Package.BluePrints.Ticket.Service import TicketService
+from Package.BluePrints.Project.Service import ProjectService
+from Package.BluePrints.User.Service import UserService
 from Package.Db import my_database
 from Package.BluePrints import API_PREFIXER, Ticker_PREFIXER
 from flask import request, jsonify , Blueprint
@@ -51,4 +53,38 @@ def getTicketDtails(ticket_id):
 @cross_origin()
 def getProjectsTicket(project_id):
     #ckeck if the project exist : 
-    return jsonify(),201
+    projectTarget = ProjectService.get_project_by_id(project_id)
+    #chcekt the result :
+    if not projectTarget["state"]:
+        return jsonify({"message": "Project not found"}),404
+    page = int(request.args.get("page", 1))
+    per_page = int(request.args.get("per_page",10))
+    #call static service to fetch all the ticket related to the project target :
+    projectTicket = TicketService.get_project_ticket(
+        project_id=project_id,
+        page=page,
+        per_page=per_page
+    )
+    return jsonify({"data":projectTicket}),201
+
+#get ticket list assigned to a user :
+@ticket_api.route(Prefixer+"/User/<user_id>", methods=["GET"])
+@cross_origin()
+def getUserTickets(user_id):
+    #check the user id :
+    userTarget = UserService.alreadyExists(user_id)
+    if not userTarget:
+        return jsonify({"message": "User not found"}),404
+    status = request.args.get("status","All")
+    if status and not status in ["Open", "Closed", "All"]:
+        return jsonify({"message": "Status not available"}),403
+    page = int(request.args.get("page", 1))
+    per_page = int(request.args.get("per_page",10))
+    
+    userTicket = TicketService.get_user_ticket(
+        user_id=user_id,
+        page=page,
+        per_page=per_page,
+        status=status
+    )
+    return jsonify({"data":userTicket}),201
