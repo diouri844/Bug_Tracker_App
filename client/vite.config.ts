83cf48476c49857b4f3945ca98b23716155c005e
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-06 22:10:47 +0200

    update vite config
    integrat Ui lib

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Component from "unplugin-vue-components/vite"
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Component({
+      resolvers:[
+        AntDesignVueResolver(
+          {
+            importStyle:false,
+          }
+        ),
+      ]
+    })
+  ],
 })
