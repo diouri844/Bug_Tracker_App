a7720d4b87d73a57aafa9f2439b7832ca58afe80
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-06 22:39:50 +0200

    setup component folder

---
+++ client/src/components/pages/login.page.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import loginComponent from "../utils/login.component.vue"
+</script>
+
+
+<template>
+    <div class="flex h-screen flex-col items-center justify-center content-start">
+        <h1 class="text-3xl font-semibold text-gray-900"> 
+            Login 
+        </h1>
+        <loginComponent />
+    </div>
+</template>
