927a0d4850e8fe9fc6aa9b88596ae69b6912eed7
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-19 20:42:40 +0200

    Setup a team page component

---
+++ client/src/components/pages/team.page.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import homeLayout from "../layouts/home.layout.vue";
+</script>
+<template>
+    <homeLayout>
+        this is my Team page :
+    </homeLayout>
+</template>
