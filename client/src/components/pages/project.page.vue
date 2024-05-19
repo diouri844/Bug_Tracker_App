24552cd6a4a9cff6137a8383fe86d384b83348e5
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-19 20:41:06 +0200

    setup a project page component

---
+++ client/src/components/pages/project.page.vue
@@ -0,0 +1,10 @@
+<template>
+    <homeLayout>
+        this is the project page :
+    </homeLayout>
+</template>
+
+
+<script lang="ts" setup>
+import homeLayout from "../layouts/home.layout.vue";
+</script>
