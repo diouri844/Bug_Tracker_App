b968cd8bc2a66ca22bfb9721725638045739b7a6
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-19 20:44:53 +0200

    Setup invitation page component

---
+++ client/src/components/pages/invitation.page.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import homeLayout from "../layouts/home.layout.vue";
+</script>
+<template>
+    <homeLayout>
+        this is my Invitation page :
+    </homeLayout>
+</template>
