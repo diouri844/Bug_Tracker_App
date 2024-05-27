de5dae66df4c713758c7dc3d691df83ede75ae3b
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-27 20:59:45 +0200

    Update home layout

--- client/src/components/layouts/home.layout.vue
+++ client/src/components/layouts/home.layout.vue
@@ -1,43 +1,53 @@
 <template>
-  <a-layout style="min-height: 100vh">
+  <a-layout style="min-height: 150vh">
     <a-layout-sider v-model:collapsed="collapsed" collapsible>
       <a-menu 
       v-model:selectedKeys="selectedKeys" 
       theme="dark" mode="inline"
       >
-        <a-menu-item key="1">
-          <pie-chart-outlined />
+        <a-menu-item key="/home">
+        <RouterLink to="/home">
+          <pie-chart-outlined class="mx-1 px-1" />
           <span>Home</span>
+        </RouterLink>
         </a-menu-item>
-        <a-menu-item key="2">
-          <desktop-outlined />
+        <a-menu-item key="/projects">
+        <RouterLink to="/projects">
+          <desktop-outlined class="mx-1 px-1"/>
           <span>Project</span>
+        </RouterLink>
         </a-menu-item>
         <a-sub-menu key="sub1">
           <template #title>
             <span>
-              <user-outlined />
+              <user-outlined class="mx-1 px-1" />
               <span>Profile</span>
             </span>
           </template>
           <a-menu-item key="3">My Account</a-menu-item>
           <a-menu-item key="5">Logout</a-menu-item>
         </a-sub-menu>
-        <a-menu-item key="sub2">
-              <team-outlined />
-              <span>Team</span>
+        <a-menu-item key="/teams">
+        <RouterLink to="/teams">
+          <team-outlined class="mx-1 px-1"/>
+          <span>Team</span>
+        </RouterLink>
         </a-menu-item>
         <a-menu-item key="9">
-          <file-outlined />
+          <file-outlined class="mx-2" />
           <span>Note</span>
         </a-menu-item>
-        <a-menu-item key="10">
-          <usergroupAddOutlined />
+        <a-menu-item key="/invitations">
+        <RouterLink to="/invitations">
+          <usergroupAddOutlined class="mx-1 px-1"/>
           <span>Invitaion</span>
+          </RouterLink>
         </a-menu-item>
-        <a-menu-item key="11">
-          <tagOutlined />
+        <a-menu-item key="/tickets">
+        <RouterLink to="/tickets">
+          <tagOutlined class="mx-1 px-1" />
           <span>Ticket</span>
+        </RouterLink>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
@@ -58,7 +68,19 @@ import {
   UsergroupAddOutlined,
   TagOutlined
 } from '@ant-design/icons-vue';
-import { ref } from 'vue';
-const collapsed = ref<boolean>(false);
-const selectedKeys = ref<string[]>(['1']);
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+const collapsed = ref<boolean>(true);
+const selectedKeys = ref<string[]>(['/home']);
+
+// setup use router api call :
+const route = useRoute();
+// time to watch :
+watch(
+  () => route.path,
+  (newPath) => {
+    selectedKeys.value = [newPath];
+  },
+  { immediate: true } // Run the watcher immediately on component mount
+);
 </script>
