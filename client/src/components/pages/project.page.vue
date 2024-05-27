23d3c58fa6af4a36ff4a80382c74bd913b60c22a
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-27 21:22:13 +0200

    update project page
    add the basic utils component

--- client/src/components/pages/project.page.vue
+++ client/src/components/pages/project.page.vue
@@ -1,10 +1,15 @@
 <template>
     <homeLayout>
+        <projectHeaderComponent />
         this is the project page :
+        <noData />
     </homeLayout>
 </template>
 
 
 <script lang="ts" setup>
 import homeLayout from "../layouts/home.layout.vue";
+//import projectTable from "../utils/project_utils/project_table.component.vue";
+import noData from "../utils/noData.component.vue";
+import projectHeaderComponent from "../utils/project_utils/project_header.component.vue";
 </script>
