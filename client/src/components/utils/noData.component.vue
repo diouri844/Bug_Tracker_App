4a80d98193648b9100ed872d722b3663b18fcb44
Chopen <75852177+diouri844@users.noreply.github.com>
2024-05-27 21:23:01 +0200

    Setup global noData found component view

---
+++ client/src/components/utils/noData.component.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full my-[10%]">
+        <a-empty :image="simpleImage" />
+        <a-button type="primary">Start new project</a-button>
+    </div>
+  </template>
+  <script lang="ts" setup>
+  import { Empty } from 'ant-design-vue';
+  const simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
+  </script>
+  
+  
